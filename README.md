e3c663c0e9ce4dc536733adf461203dc077ee732
Batista <550461053@qq.com>
2024-12-09 20:28:46 +0800

    refine code

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ calculator for pytorch model
 
 ## Attention
 1. MHA/MQA/QGA
+
+```sh
+python mha.py
+
+# it will show:
+# test MHA：
+# input size: torch.Size([128, 1024, 8192])
+# output size: torch.Size([128, 1024, 8192])
+# cost_time: 12.70s
+# test MQA：
+# input size: torch.Size([128, 1024, 8192])
+# output size: torch.Size([128, 1024, 8192])
+# cost_time: 7.94s
+# test GQA：
+# input size: torch.Size([128, 1024, 8192])
+# output size: torch.Size([128, 1024, 8192])
+# cost_time: 8.35s
+```
+
--- mha.py
+++ mha.py
@@ -219,18 +219,6 @@ def test_mha(attn='MHA'):
     
 
 if __name__ == "__main__":
-    # test_mha("MHA")
-    # test_mha("MQA")
+    test_mha("MHA")
+    test_mha("MQA")
     test_mha("GQA")
-    # test MHA：
-    # input size: torch.Size([128, 1024, 8192])
-    # output size: torch.Size([128, 1024, 8192])
-    # cost_time: 12.70s
-    # test MQA：
-    # input size: torch.Size([128, 1024, 8192])
-    # output size: torch.Size([128, 1024, 8192])
-    # cost_time: 7.94s
-    # test GQA：
-    # input size: torch.Size([128, 1024, 8192])
-    # output size: torch.Size([128, 1024, 8192])
-    # cost_time: 8.35s
